062b04a5a278615067846bce73e3b3d276717a0d
tanya-ling <tanya96gavrilova@yandex.ru>
2018-10-31 22:46:40 +0300

    plotting distances

--- distance_calculation.py
+++ distance_calculation.py
@@ -1,5 +1,6 @@
 from prepare_data import read_csv_data
 import numpy
+import pandas
 
 
 def splitting(row):
@@ -26,29 +27,39 @@ def book2_distance(book_title1, book_title2):
     return D
 
 
-def book2allbooks_distances(book_title):
-    dict = {}
+def book2allbooks_distances(book_title, number):
+    dic = {}
     for a in db.index:
         if a != book_title:
             D = book2book_distance(a, book_title)
-            dict[a] = D
-    return dict
+            dic[a] = D
+    list_pairs = [0 for i in range(number)]
+    i = 0
+    for key, value in sorted(dic.items(), key = lambda x : x[1]):
+        list_pairs[i] = [key, value]
+        i += 1
+        if i >= number:
+            return list_pairs
 
 
-def distance_matrix_creation(sourse_path, results_path):
+def closest_for_every_book(results_path, number=10):
+    dic = {}
+    for book_pr in db.index:
+        dic[book_pr] = book2allbooks_distances(book_pr, number)
+    ndf = pandas.DataFrame.from_dict(dic, orient='index')
+    ndf.index.name = 'book_title'
+    ndf.to_csv(results_path, sep='|', index=True)
+
+
+if __name__ == "__main__":
+    # print(book2book_distance('A Court of Wings and Ruin', 'American War'))
+    # print(book2book_distance('American War', 'American War'))
+    # print(book2book_distance('Wildfire (Hidden Legacy, #3)', 'White Hot (Hidden Legacy, #2)'))
+    # dic = book2allbooks_distances('Wildfire (Hidden Legacy, #3)', 3)
+    # print(sorted( ((v,k) for k,v in dic.items()), reverse=False))
+    sourse_path = '2017_books_v5_preproc_v4_agreg_v1_keywords_v5.csv'
     db = read_csv_data(sourse_path, '|', rew=False)
     db['keyword_vector'] = db.apply(splitting, axis=1)
     print(db.head())
     db = db.groupby('book_title')['keyword_vector'].agg(list)
-
-if __name__ == "__main__":
-    db = read_csv_data('2017_books_v5_preproc_v4_agreg_v1_keywords_v5.csv', '|', rew=False)
-    db['keyword_vector'] = db.apply(splitting, axis=1)
-    print(db.head())
-    db = db.groupby('book_title')['keyword_vector'].agg(list)
-    print(db.head())
-    print(book2book_distance('A Court of Wings and Ruin', 'American War'))
-    print(book2book_distance('American War', 'American War'))
-    print(book2book_distance('Wildfire (Hidden Legacy, #3)', 'White Hot (Hidden Legacy, #2)'))
-    dict = book2allbooks_distances('Wildfire (Hidden Legacy, #3)')
-    print(sorted( ((v,k) for k,v in dict.items()), reverse=False))
+    closest_for_every_book('2017_books_v5_preproc_v4_agreg_v1_keywords_v5_distances_v1.csv')
---
+++ plotting_closest.py
@@ -0,0 +1,50 @@
+import matplotlib.pylab as plt
+from prepare_data import read_csv_data
+from random import uniform
+from math import pi, sin, cos, log, sqrt
+from ast import literal_eval
+
+
+def bold_title(title):
+    title = title.replace('#', '\#')
+    return r"$\bf{" + title + "}$"
+
+
+def creater_coordinates(data, title):
+    dic = {bold_title(title): [0, 0]}
+    angle_r = 0.3 * pi
+    for item in data:
+        angle = uniform(0, 2) * pi
+        d = log(literal_eval(item)[1])
+        d = literal_eval(item)[1]
+        d = sqrt(literal_eval(item)[1] )
+        dic[literal_eval(item)[0].replace('(', '\n(')] = [d * sin(angle), d * cos(angle)]
+        dic[literal_eval(item)[0].replace('(', '\n(')] = [d * sin(angle_r), d * cos(angle_r)]
+        angle_r += 0.2 * pi
+    return dic
+
+
+def plotting(test, book_title):
+    xs, ys = zip(*test.values())
+    labels = test.keys()
+
+    plt.figure(figsize=(8, 10))
+    plt.title("The closest books to: " + bold_title(book_title), fontsize=20)
+    plt.scatter(xs, ys, marker='o')
+    plt.axis('scaled')
+    for label, x, y in zip(labels, xs, ys):
+        plt.annotate(label, xy=(x, y), ha='center')
+    # plt.axis('off')
+    plt.xticks([])
+    plt.yticks([])
+    plt.tight_layout()
+    plt.savefig('foo.png')
+
+
+if __name__ == "__main__":
+    sourse_path = '2017_books_v5_preproc_v4_agreg_v1_keywords_v5_distances_v1.csv'
+    db = read_csv_data(sourse_path, '|', rew=False)
+    db.set_index('book_title', inplace=True)
+    title = 'White Hot (Hidden Legacy, #2)'
+    dic = creater_coordinates(db.loc[title].values, title)
+    plotting(dic, title)
